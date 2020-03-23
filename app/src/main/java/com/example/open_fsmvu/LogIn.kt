7cb458227bfaefec0d890106f4a951027a40499d
Yusuf Kaya <myusufka@gmail.com>
2020-03-23 15:34:21 +0300

    first commit

---
+++ app/src/main/java/com/example/open_fsmvu/LogIn.kt
@@ -0,0 +1,12 @@
+package com.example.open_fsmvu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
